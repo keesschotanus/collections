83ded4a7ba71305189b8fa0be069af95629d6fc3
keesschotanus <kees.schotanus@planet.nl>
2026-06-27 08:40:58 +0200

    Worked on unit tests
    Added new tests for hash and chunk

--- test/main.c
+++ test/main.c
@@ -1,22 +1,16 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 void test_chunk(void);
-void test_hash(void);
-
 void test_list(void);
 void test_doubly_linked_list(void);
-
 void test_hash_map(void);
-
 void test_binary_tree(void);
 void test_binary_search_tree(void);
 
 int main(void)
 {
 	test_chunk();
-	test_hash();
-
 	test_list();
 	test_doubly_linked_list();
 	
@@ -25,8 +19,6 @@ int main(void)
 	test_binary_tree();
 	test_binary_search_tree();
 	
-	puts("All tests have passed");
-
 	return 0;
 }
 
